ff800c4750db2d22a699214be05bc97f37a7a01c
Madumarov Davron <menaceteacher@gmail.com>
2025-06-18 11:06:24 +0500

    add

--- src/main/java/org/example/onlineshop9a/model/User.java
+++ src/main/java/org/example/onlineshop9a/model/User.java
@@ -1,10 +1,7 @@
 package org.example.onlineshop9a.model;
 
 import jakarta.persistence.*;
-import lombok.*;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 public class User {
     @Id
@@ -16,4 +13,56 @@ public class User {
     private String password;
     @Column(nullable = false, unique = true)
     private String email;
+
+    public User(Long userid, String username, String password, String email) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public User() {
+    }
 }
